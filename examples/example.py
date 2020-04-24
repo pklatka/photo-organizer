574193552044662d1e4fe48b7b7e21b3e4029136
pklatka <44744462+pklatka@users.noreply.github.com>
2020-04-24 19:50:26 +0200

    Add example files

---
+++ examples/example.py
@@ -0,0 +1,13 @@
+from PhotoSegregator.PhotoSegregator import *
+from datetime import date
+
+date_ranges = [
+    ('Paris 2007', date(2007, 9, 16), date(2007, 9, 16)),
+    ('Paryż 2008', date(2007, 9, 16), date(2007, 9, 17)),
+]
+
+root_path = r"C:\Users\Test\Desktop\dest_dir"
+dest_path = r'C:\Users\Test\Desktop\backup'
+
+print(get_root_path_file_number(root_path))
+print(copy_files_by_ranges(root_path,dest_path,date_ranges,save_unsorted=False))
