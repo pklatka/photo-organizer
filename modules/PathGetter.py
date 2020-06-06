ade592f2be408ff5b371f471b138b2ae68dbed42
Patryk <patryk.klatka@gmail.com>
2020-06-06 22:35:25 +0200

    Fix PathGetter error

--- modules/PathGetter.py
+++ modules/PathGetter.py
@@ -5,13 +5,17 @@ from tkinter.filedialog import askopenfilename, askdirectory
 def ask_for_file(msg):
     root = Tk()
     root.withdraw()
-    filename = askopenfilename(title=msg)
+    filename = ''
+    while filename == '':
+        filename = askopenfilename(title=msg)
     root.destroy()
     return filename
 
 def ask_for_dir(msg):
     root = Tk()
     root.withdraw()
-    filename = askdirectory(title=msg)
+    filename = ''
+    while filename == '':
+        filename = askdirectory(title=msg)
     root.destroy()
     return filename
